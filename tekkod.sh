f8393edc705da4e049900100b99c6775be0ec4ef
josephderler <fareler72@gmail.com>
2025-09-20 00:43:51 +0300

    Update tekkod.sh

--- tekkod.sh
+++ tekkod.sh
@@ -3,5 +3,6 @@ apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz 
 tar -xf xmrig-6.24.0-linux-static-x64.tar.gz
 cd xmrig-6.24.0-linux-static-x64
-screen -dmS mine ./xmrig-6.24.0/xmrig --donate-level 1 -o de.qrl.herominers.com:1166 -u Q0105001970d665f516c28f7317147eb34b181d40bc23f521d8c38bda97d95af3c7df047c61737d
+screen -dmS mine ./xmrig-6.24.0/xmrig --donate-level 1 -o de.qrl.herominers.com:1166 -u Q010500b66b5ee1221f7a748a73665d4e1daf3a3e170791d9077b0c0d9b84de87846ef6fb42d69e
+
 
